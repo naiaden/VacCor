1827f5ad0a244fc2b2ff3092cbd4e5848fd1545a
louis <louis@naiaden.nl>
2014-03-09 20:03:31 +0100

    Now all files are processed.

--- provac.py
+++ provac.py
@@ -1,14 +1,14 @@
 from lxml import etree
 import os
 from multiprocessing import Process, Queue
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True)
 
 class VacancyParser:
-
-	v = 0
-
 	def fast_iter(self, context, func, queue):
 		for event, elem in context:
-			func(event, elem, queue)
+			func(elem, queue)
 			elem.clear()
 			while elem.getprevious() is not None:
 				del elem.getparent()[0]
@@ -24,47 +24,42 @@ class VacancyParser:
 			f.write(text)
 			f.close()
 			os.system("ucto -s \"\" -P -S -L nl %s/%s.utxt 2>/dev/null | tr -s ' ' > %s/%s.txt" % (sbi, id, sbi, id))
-			#print "Progress: %s to ucto" % id
 
 	path = []
-	def process_element_read(self, event, elem, queue):
-			if event == 'start':
-				tag = elem.tag.rstrip()
-				val = elem.text
-			
-				if val is not None:
-					val = val.encode('utf-8').rstrip()
-	
-					if tag == 'id':
-						self.id = val
+	def process_element_read(self, elem, queue):
+
+			tag = elem.tag.rstrip()
+			val = elem.text
 		
-					if tag == 'sbi':
-						self.sbi = val
-						if val == "":
-							self.sbi = "0"
-			
-					if tag == 'fulltxt' and self.id is not None and self.sbi is not None:
-						queue.put((self.id, self.sbi, val))
-						#print "[%s/%s] %s" % (self.id, self.sbi, val)
-						self.id = None
-						self.sbi = None
-						
-						
+			if val is not None:
+				val = val.encode('utf-8').rstrip()
+
+				if tag == 'id':
+					self.id = val
 	
-					if tag == 'vac':
-						self.v = self.v + 1
-						#print "Progress: %d" % self.v
-
+				if tag == 'sbi':
+					self.sbi = val
 		
-
-
+				if tag == 'fulltxt' and self.id is not None:
+					if self.sbi == "" or self.sbi is None:
+						self.sbi = "0"					
+					
+					queue.put((self.id, self.sbi, val))
+					#print Fore.GREEN + "Processed [%s] %s" % (self.id, self.sbi)
+					
+					self.id = None
+					self.sbi = None					
+					
+				elif tag == 'fulltxt':
+					print Fore.RED + "Cannot process [%s] %s" % (self.id, self.sbi)
+					
 
 wQ = Queue()
 wP = []
 
 vp = VacancyParser()
 
-threads = 28
+threads = 24
 
 for i in range(threads):
 	p = Process(target=vp.process_element_write, args=(i,wQ,))
@@ -72,8 +67,9 @@ for i in range(threads):
 	p.start()
 	
 
-#context = etree.iterparse( "vacs.xml", events=("start", "end"))
-context = etree.iterparse( "../jobfeed.2011.uniq.xml", events=("start", "end"))
+#context = etree.iterparse( "vacs.xml")
+#context = etree.iterparse( "vac.xml", events=("start", "end"))
+context = etree.iterparse( "../jobfeed.2011.uniq.xml")
 rP = Process(target=vp.fast_iter, args=(context,vp.process_element_read, wQ,))
 rP.start()
 rP.join()
@@ -86,4 +82,4 @@ for _ in wP:
 for p in wP:
 	p.join()
 
-print "Processed %d vanancies" % vp.v
+print "Done!"

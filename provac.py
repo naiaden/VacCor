65cb68af5ba7407cc11f0ab123b1932c2c346451
louis <louis@naiaden.nl>
2014-03-10 11:44:58 +0100

    Cmd-line arguments implemented.

--- provac.py
+++ provac.py
@@ -1,11 +1,57 @@
 from lxml import etree
 import os
+import sys
 from multiprocessing import Process, Queue
 from colorama import init, Fore, Back, Style
-
-init(autoreset=True)
+import getopt
 
 class VacancyParser:
+	use_ucto = True
+	extract = True
+	default_sbi = "nosbi"
+	threads = 24
+	input = ""
+
+	def printHelp(self):
+		print "Vacancy Parser v0.1 -- louis@naiaden.nl\n"
+		print "Extracts vacancies from an xml file, and flattens them into tokenised files"
+		print "Arguments for provac.py"
+		print "-h, --help                   Prints this help and exits"
+		print "-d, --dir <string>           The output directory. Default the files are saved in a directory corresponding to their sbi"
+		print "-U, --no-ucto                Skip the tokenisation step"
+		print "-E, --no-extract             Skip the extraction step"
+		print "-s, --default-sbi <string>   The value for unknown sbi. Default is nosbi (not used in combination with -d)"
+		print "-t, --threads <n>            Number of threads. Default is 24"
+
+	def __init__(self, cmdArgs):
+		try:
+			opts, args = getopt.getopt(cmdArgs, 'hd:UEs:t:', ['help', 'dir=', 'no-ucto', 'no-extract', 'default-sbi=', 'threads='])
+		except getopt.GetoptError:
+			self.printHelp()
+			sys.exit(2)
+			
+		for (opt, arg) in opts:
+			if opt in ('-h', '--help'):
+				self.printHelp()
+				sys.exit()
+			if opt in ('-d', '--dir'):
+				self.dir = arg
+			if opt in ('-U', '--no-ucto'):
+				self.use_ucto = False
+			if opt in ('-E', '--no-extract'):
+				self.extract = False
+			if opt in ('-s', '--default-sbi'):
+				self.extract = False
+			if opt in ('-t', '--threads'):
+				self.threads = arg
+			if opt in ('-f', '--input'):
+				self.input = arg
+		
+		init(autoreset=True)
+		
+		if not self.input:
+			print "No input is provided. Program halts"
+
 	def fast_iter(self, context, func, queue):
 		for event, elem in context:
 			func(elem, queue)
@@ -20,12 +66,13 @@ class VacancyParser:
 			if not id and not sbi and not text:
 				return
 			
-			f = open("%s/%s.utxt" % (sbi, id), 'w')
-			f.write(text)
-			f.close()
-			os.system("ucto -s \"\" -P -S -L nl %s/%s.utxt 2>/dev/null | tr -s ' ' > %s/%s.txt" % (sbi, id, sbi, id))
+			if self.flatten:
+				f = open("%s/%s.utxt" % (sbi, id), 'w')
+				f.write(text)
+				f.close()
+			if self.use_ucto:
+				os.system("ucto -s \"\" -P -S -L nl %s/%s.utxt 2>/dev/null | tr -s ' ' > %s/%s.txt" % (sbi, id, sbi, id))
 
-	path = []
 	def process_element_read(self, elem, queue):
 
 			tag = elem.tag.rstrip()
@@ -42,7 +89,7 @@ class VacancyParser:
 		
 				if tag == 'fulltxt' and self.id is not None:
 					if self.sbi == "" or self.sbi is None:
-						self.sbi = "0"					
+						self.sbi = "nosbi"					
 					
 					queue.put((self.id, self.sbi, val))
 					#print Fore.GREEN + "Processed [%s] %s" % (self.id, self.sbi)
@@ -53,33 +100,35 @@ class VacancyParser:
 				elif tag == 'fulltxt':
 					print Fore.RED + "Cannot process [%s] %s" % (self.id, self.sbi)
 					
+	def run(self):
+		wQ = Queue()
+		wP = []
+		
+		for i in range(vp.threads):
+			p = Process(target=vp.process_element_write, args=(i,wQ,))
+			wP.append(p)
+			p.start()
+		
+		context = etree.iterparse( "../jobfeed.2011.uniq.xml")
+		rP = Process(target=vp.fast_iter, args=(context,vp.process_element_read, wQ,))
+		rP.start()
+		rP.join()
 
-wQ = Queue()
-wP = []
+		print "Done reading the XML file"
 
-vp = VacancyParser()
+		for _ in wP:
+			wQ.put((None, None, None))
 
-threads = 24
+		for p in wP:
+			p.join()
 
-for i in range(threads):
-	p = Process(target=vp.process_element_write, args=(i,wQ,))
-	wP.append(p)
-	p.start()
+		print "Done!"
+
+
+vp = VacancyParser(sys.argv[1:])
+vp.run()
+
+
 	
 
-#context = etree.iterparse( "vacs.xml")
-#context = etree.iterparse( "vac.xml", events=("start", "end"))
-context = etree.iterparse( "../jobfeed.2011.uniq.xml")
-rP = Process(target=vp.fast_iter, args=(context,vp.process_element_read, wQ,))
-rP.start()
-rP.join()
 
-print "Done reading the XML file"
-
-for _ in wP:
-	wQ.put((None, None, None))
-
-for p in wP:
-	p.join()
-
-print "Done!"
